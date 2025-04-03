6d4f3ccd1b31cd99734dbf3490eac6ace77c279f
Mokshith <mlolakap@charlotte.edu>
2025-04-03 21:59:26 +0530

    Added Environments for API's

---
+++ Frontend/src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:8888/api',
+    firebase: {
+      apiKey: "AIzaSyC60x-l8q4674Hb31HQ",
+      authDomain: "zero-............-waste.firebaseapp.com",
+      projectId: "zero-........-waste",
+      storageBucket: "zero-..............-waste.firebasestorage.app",
+      messagingSenderId: "878.....942907",
+      appId: "1:878484................df19236a07e834b1ac1e",
+      vapidKey: "BMwMi4oOSDGUGu2JtX.................................vZB3HZWuga43Mgzd56d8f58pT66ZBZyczBGg"
+    }
+  };
---
+++ Frontend/src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://zero-waste.devhost.my/api',
+  firebase: {
+    apiKey: "AIzaSyC60x-l8q4674Hb31HQ",
+    authDomain: "zero-............-waste.firebaseapp.com",
+    projectId: "zero-........-waste",
+    storageBucket: "zero-..............-waste.firebasestorage.app",
+    messagingSenderId: "878.....942907",
+    appId: "1:878484................df19236a07e834b1ac1e",
+    vapidKey: "BMwMi4oOSDGUGu2JtX.................................vZB3HZWuga43Mgzd56d8f58pT66ZBZyczBGg"
+  }
+};
+
+
